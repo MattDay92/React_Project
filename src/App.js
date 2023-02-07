9ded3ad254082b46096928b8390f9a2db685ef71
Matt Day <mattday@matts-mac-mini.lan>
2023-02-07 16:42:20 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Nav from './Nav';
+import Login from './Login';
+import Logout from './Logout';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(){
+    super();
+  }
+  render() {
+    return (
+      <div>
+      <Nav />
+      <br/>
+      <Login />
+      <br/>
+      <Logout />
+      </div>
+    )
+  }
 }
 
-export default App;
---
+++ src/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Login extends Component {
+  render() {
+    return (
+        <div className='container'>
+        <h1 className='row d-flex justify-content-center'>Login</h1>
+        <div className='row d-flex justify-content-center my-5'>
+            <form className='col-3 text-center'>
+                <input type='text' className='form-control' placeholder='Username'></input>
+                <input type='text' className='form-control' placeholder='Passholder'></input>
+                <button type='submit' className='btn btn-primary'>Submit</button>
+            </form>
+        </div>
+    </div>
+    )
+  }
+}
