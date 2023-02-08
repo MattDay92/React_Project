42d2e8b4fd88ec9835c938f256d161249c77b91a
Matt Day <mattday@matts-mac-mini.lan>
2023-02-08 17:04:57 -0500

    Wednesday Commit

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react'
 
 export default class ToDoItem extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
     }
+
     render() {
         return (
+            <>
             <div className='d-flex justify-content-center my-2 text-center'>
                 <div className="card" style={{width: '18rem'}}>
                     <div className="card-body">
                         <h5 className="card-title">{this.props.itemInfo.todo}</h5>
                         <h6 className="card-subtitle mb-2 text-muted">{this.props.itemInfo.day}</h6>
-                        <a href="#" className="card-link">Mark as Done</a>
+                        <a href="#" className="card-link text-primary">Mark as Done</a>
                     </div>
                 </div>
             </div>
+            </>
         )
     }
 }
--- src/views/ToDo.js
+++ src/views/ToDo.js
@@ -2,30 +2,27 @@ import React, { Component } from 'react'
 import ToDoItem from '../components/ToDoItem'
 
 export default class ToDo extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      info: [{
-        todo: 'Mow the Yard',
-        day: 'Monday'
-      }, {
-        todo: 'Rake the Leaves',
-        day: 'Tuesday'
-      }]
+      info: [],
+      new_info: ''
     }
   }
 
   showToDo = () => {
     console.log('before showToDo')
-    this.state.info.map((c) => { return <ToDoItem itemInfo={c} /> })
-    console.log('after showToDo')
+    return this.state.info.map((c) => { return <ToDoItem itemInfo={c} /> })
   };
 
   addToList = (e) => {
     e.preventDefault()
     const todo = e.target.todoitem.value
+    console.log(this)
     const day = e.target.day.value
-    this.setState([{todo: todo, day: day}])
+    this.setState(this.state.info = [...this.state.info, { todo: todo, day: day}])
+    this.showToDo()
+    console.log(this.state.info)
     console.log(`Added ${todo} and ${day} to info.`)
   };
 
@@ -35,14 +32,14 @@ export default class ToDo extends Component {
       <div className='container col-4'>
         <div className='row my-5'>
           <h1 className='text-center'>Add to To-Do List</h1>
-          <form onSubmit={this.addToList}>
+          <form className='text-center' onSubmit={this.addToList}>
             <input className='form-control' type='text' placeholder='To-Do' name='todoitem'/>
             <input className='form-control' type='text' placeholder='Day' name='day'/>
             <button className='btn btn-primary' type='submit'>Add to List</button>
           </form>
         </div>
         <div className='row'>
-          {this.state.info.length === 0?<p>Nothing To-Do Yet</p>:this.showToDo()}
+          {this.state.info.length === 0?<p className='text-center'>Nothing To-Do Yet...</p>:this.showToDo()}
         </div>
       </div>
     )
