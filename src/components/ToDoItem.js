ad38fae4b08aa8abc132a5df68e18145a0a7010f
Matt Day <mattday@matts-mac-mini.lan>
2023-02-08 16:22:26 -0500

    third commit

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -6,11 +6,11 @@ export default class ToDoItem extends Component {
     }
     render() {
         return (
-            <div>
+            <div className='d-flex justify-content-center my-2 text-center'>
                 <div className="card" style={{width: '18rem'}}>
                     <div className="card-body">
                         <h5 className="card-title">{this.props.itemInfo.todo}</h5>
-                        <h6 className="card-subtitle mb-2 text-muted">{this.props.dayInfo.day}</h6>
+                        <h6 className="card-subtitle mb-2 text-muted">{this.props.itemInfo.day}</h6>
                         <a href="#" className="card-link">Mark as Done</a>
                     </div>
                 </div>
--- src/views/ToDo.js
+++ src/views/ToDo.js
@@ -5,13 +5,19 @@ export default class ToDo extends Component {
   constructor() {
     super();
     this.state = {
-      info: []
+      info: [{
+        todo: 'Mow the Yard',
+        day: 'Monday'
+      }, {
+        todo: 'Rake the Leaves',
+        day: 'Tuesday'
+      }]
     }
   }
 
   showToDo = () => {
     console.log('before showToDo')
-    return this.state.info.map((c) => { return <ToDoItem itemInfo={c} /> })
+    this.state.info.map((c) => { return <ToDoItem itemInfo={c} /> })
     console.log('after showToDo')
   };
 
@@ -19,15 +25,16 @@ export default class ToDo extends Component {
     e.preventDefault()
     const todo = e.target.todoitem.value
     const day = e.target.day.value
-    this.setState({info: {todo: todo, day: day}})
+    this.setState([{todo: todo, day: day}])
     console.log(`Added ${todo} and ${day} to info.`)
   };
 
 
   render() {
     return (
-      <div className='container col-8'>
-        <div className='row'>
+      <div className='container col-4'>
+        <div className='row my-5'>
+          <h1 className='text-center'>Add to To-Do List</h1>
           <form onSubmit={this.addToList}>
             <input className='form-control' type='text' placeholder='To-Do' name='todoitem'/>
             <input className='form-control' type='text' placeholder='Day' name='day'/>
