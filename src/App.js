c8f432f57bb31e36d17fd4b8b628568d383fee77
Matt Day <mattday@day-macbook-pro.lan>
2023-02-09 15:34:46 -0500

    Thursday Commit 1

--- src/App.js
+++ src/App.js
@@ -3,13 +3,38 @@ import Nav from './components/Nav';
 import Login from './views/Login';
 import Logout from './views/Logout';
 import ToDo from './views/ToDo'
+import ToDoItem from './components/ToDoItem';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 
 export default class App extends Component {
   constructor() {
     super();
+    this.state = {
+      info: [],
+      new_info: ''
+    }
   }
+
+  addToList = (e) => {
+    e.preventDefault()
+    const todo = e.target.todoitem.value
+    console.log(this)
+    const day = e.target.day.value
+    this.setState(this.state.info = [...this.state.info, { todo: todo, day: day}])
+    console.log(this.state.info)
+    console.log(`Added ${todo} and ${day} to info.`)
+  };
+
+  showToDo = () => {
+    console.log('before showToDo')
+    return this.state.info.map((c, i) => { return <ToDoItem key={i} itemInfo={c} /> })
+  };
+
+  removeFromList = () => {
+      this.setState({info: []})
+  }
+
   render() {
     return (
       <BrowserRouter>
@@ -18,7 +43,7 @@ export default class App extends Component {
           <Routes>
             <Route path='/login' element={<Login />} />
             <Route path='/logout' element={<Logout />} />
-            <Route path='/todo' element={<ToDo />} />
+            <Route path='/todo' element={<ToDo info={this.state.info} addToList={this.addToList} showToDo={this.showToDo} removeFromList={this.removeFromList} />} />
           </Routes>
         </div>
       </BrowserRouter>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,44 +1,212 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+// import icon from './images/ToDo_icon.png'
 
-export default class Nav extends Component {
-    render() {
-        return (
-            <div>
-                <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="#">
-                        <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" width="30" height="24"/>
-                            Navbar</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                            <ul className="navbar-nav">
-                                <li className="nav-item">
-                                    <Link className="nav-link active" aria-current="page" to="todo">Home</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/login">Login</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/logout">Logout</Link>
-                                </li>
-                                <li className="nav-item dropdown">
-                                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Dropdown link
-                                    </a>
-                                    <ul className="dropdown-menu">
-                                        <li><a className="dropdown-item" href="#">Action</a></li>
-                                        <li><a className="dropdown-item" href="#">Another action</a></li>
-                                        <li><a className="dropdown-item" href="#">Something else here</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-            </div>
-        )
-    }
+// export default class Nav extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <nav className="navbar navbar-expand-lg bg-body-tertiary">
+//                     <div className="container-fluid">
+//                         <a className="navbar-brand" href="#">
+//                         <img src={icon} width="30" height="24"/>
+//                             </a>
+//                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+//                             <span className="navbar-toggler-icon"></span>
+//                         </button>
+//                         <div className="collapse navbar-collapse" id="navbarNavDropdown">
+//                             <ul className="navbar-nav">
+//                                 <li className="nav-item">
+//                                     <Link className="nav-link active" aria-current="page" to="todo">To Do</Link>
+//                                 </li>
+//                                 <li className="nav-item">
+                                    // <Link className="nav-link" to="/login">Login</Link>
+//                                 </li>
+//                                 <li className="nav-item">
+                                    // <Link className="nav-link" to="/logout">Logout</Link>
+//                                 </li>
+//                                 <li className="nav-item dropdown">
+//                                     <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//                                         Dropdown link
+//                                     </a>
+//                                     <ul className="dropdown-menu">
+//                                         <li><a className="dropdown-item" href="#">Action</a></li>
+//                                         <li><a className="dropdown-item" href="#">Another action</a></li>
+//                                         <li><a className="dropdown-item" href="#">Something else here</a></li>
+//                                     </ul>
+//                                 </li>
+//                             </ul>
+//                         </div>
+//                     </div>
+//                 </nav>
+//             </div>
+//         )
+//     }
+// }
+
+
+
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import icon from './images/ToDo_icon.png'
+
+
+
+const pages = [
+<Link className="nav-link active text-white" aria-current="page" to="todo">To Do</Link>, 
+<Link className="nav-link text-white" to="/login">Login</Link>, 
+<Link className="nav-link text-white" to="/logout">Logout</Link>
+];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function ResponsiveAppBar() {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        LOGO
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">{page}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href=""
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        LOGO
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                <Avatar alt="Remy Sharp" src={icon} />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 }
+export default ResponsiveAppBar;
